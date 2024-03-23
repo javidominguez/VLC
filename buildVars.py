185d564f4a7614eb0884d55a1d4a286083b5711f
Javi Domínguez <fjavids@gmail.com>
2024-03-23 22:15:34 +0100

    Release 2024.1.0 stable version

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Provides some accessibility features for VLC Media Player, as improve navigation  or read status bar"""),
 	# version
-	"addon_version": "2024.0.2",
+	"addon_version": "2024.1.0",
 	# Author(s)
 	"addon_author": "Javi Dominguez <fjavids@gmail.com>",
 	# URL for the add-on documentation support
